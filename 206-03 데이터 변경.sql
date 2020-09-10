79c8916b68c8b0c5f7b3b1a1021b1f7c60a3e0ce
Wonhyuk Chung <wonhyukc@users.noreply.github.com>
2020-09-11 07:17:13 +0900

    주석 추가

--- 206-03 데이터 변경.sql
+++ 206-03 데이터 변경.sql
@@ -5,6 +5,10 @@
 	
 	https://github.com/wonhyukc/mySQL
 */
+/*
+	실행하기 전/후에 "206-01 HRDB 데이터베이스.sql" 을 실행해서 깨끗한 HRDB를 만들어 두고 진행하세요.
+    이 실습을 하면 데이터가 달라집니다. 
+*/
 
 use HRDB;
 SELECT database();
@@ -92,17 +96,31 @@ SELECT *
 -- 2) FROM 절을 사용한 다양한 조건 지정
 select * from Employee;
 SELECT EmpID, COUNT(*) as cnt	FROM Vacation GROUP BY  EmpID  HAVING count(*) > 2;
+--  "206-04 데이터 집계.sql" 를 배워야 이해할 수 있다.
+select * from Employee where empid = 's0001';
+-- salary 값을 기억해두자 : 8500
 
 UPDATE Employee a
 	JOIN (
 		SELECT EmpID, COUNT(*) as cnt	FROM Vacation GROUP BY  EmpID  HAVING count(*) > 2
     ) b ON a.EmpID = b.EmpID	
 SET Salary = Salary * 0.8;
-/*
-Error Code: 1175. You are using safe update mode and you tried to update a table without a WHERE that uses a KEY column 
-To disable safe mode, toggle the option in Preferences -> SQL Queries and reconnect.	0.000 sec
-*/
 
+select * from Employee;
+select * from Employee where empid = 's0001';
+-- salary 값을 비교해보자. : 8500 > 6800
+
+
+UPDATE Employee a
+SET RetireDate = now();
+/*
+다음 오류 메시지가 나오면 
+SET RetireDate = now()	Error Code: 1175. You are using safe update mode and you tried to update a table without a WHERE that uses a KEY column. 
+ To disable safe mode, toggle the option in Preferences -> SQL Editor and reconnect.
+ 
+이걸 해야 한다.
+Edit > Preferences > SQL Editor > 제일 밑 Safe Updates 체크 해제 > 재 접속
+*/
 select * from Employee;
 
 
@@ -113,9 +131,11 @@ select * from Employee;
 
 -- 1) 기본적인 DELETE문
 
+SELECT * FROM Vacation;
 DELETE from Vacation
    WHERE VacationID = 10;
-
+SELECT * FROM Vacation;
 -- TRUNCATE TABLE
 
 TRUNCATE TABLE Vacation;
+SELECT * FROM Vacation;

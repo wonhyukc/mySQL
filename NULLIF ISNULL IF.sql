a0b7dfb42e8f5e4eff638f2e36adea99d7e2bac6
Wonhyuk Chung <wonhyukc@users.noreply.github.com>
2020-09-11 10:17:43 +0900

    추가

--- NULLIF ISNULL IF.sql
+++ NULLIF ISNULL IF.sql
@@ -5,7 +5,11 @@ SELECT NULLif ( 3, 3);
 -- NULL
 SELECT NULLif ( 3, 2);
 -- 3
+select nullif(컬럼, '무응답') from 테이블;
 
+-- 참/ 거짓
+-- 1 = 참, TRUE
+-- 0 = 거짓, FALSE
 
 -- ISNULL(값1) 
 -- 값1이 NULL 이면 1, 아니면 0 을 리턴
@@ -13,12 +17,11 @@ SELECT isNull ( 3) ;
 -- 0
 SELECT isNull ( NULL );
 -- 1
-
+select isnull(컬럼)  from 테이블;
 
 -- IF ( 조건, 참일때값, 거짓일때값)
--- SELECT IF( '가위' ==  '바위', '다르다', '같다');
 SELECT IF( 5 < 3, '맞다', '틀리다') ;
-SELECT IF( 1 < 3, '맞다', '틀리다') ;
+SELECT IF( 1 < 3, '맞다', '틀리다')  as 결과;
 
 
 -- ISNULL 과 IF 를 같이 사용하면

94774cf7f9f2f36db6225f4bf2dce034615e3e98
Wonhyuk Chung <wonhyukc@users.noreply.github.com>
2020-09-16 16:28:07 +0900

    추가

---
+++ 206-06 연습01.sql
@@ -0,0 +1,23 @@
+
+USE HR;
+select * from job_history
+;
+select * from employees
+;
+
+
+-- 부서 이동 경력이 있는 사람만 
+select distinct e.*  from job_history jh
+	join employees e on jh.employee_id = e.employee_id
+;    
+-- subquery
+select * from employees
+where employee_id in (select employee_id from job_history)
+;    
+    
+-- 부서 이동 경력이 없는 사람만 
+select e.*  
+from job_history jh
+	right join employees e on jh.employee_id = e.employee_id
+where jh.employee_id is null
+;
